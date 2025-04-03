36693dd23b059dbbca1e4c29c18b05406a7e11ad
Ledrooo <seciek13@gmail.com>
2025-04-03 15:18:12 +0200

    15. Dodanie komentarze o poprawności programu

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -3,6 +3,7 @@ package edu.kis.vh.nursery;
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
+//Projekt jest poprawny, testy jednostkowe przebiegły pomyślnie
 class RhymersDemo {
 
     private static final int MAX_COUNT = 15;
